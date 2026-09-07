75ce0ab7c18d9e7322f98d217ae8069a9ecfafd6
OAT [Sarun] <srnkhumthai@gmail.com>
2026-09-08 01:44:32 +0700

    fix(build): install tw-animate-css and fix @radix-ui/react-select import

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from "lucide-react"
-import { Select as SelectPrimitive } from "radix-ui"
+import * as SelectPrimitive from "@radix-ui/react-select"
 
 function Select({
   ...props
